969af5cb2d58a854e74dbb38fe68d82f41834a5c
Dilip Sanchora <sanchoradilip@gmail.com>
2022-10-27 16:13:06 +0530

    refactoring in screens and widgets

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -4,8 +4,10 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:harbinger/widgets/loader_widget.dart';
 import 'package:process_run/shell.dart';
 import 'dart:developer';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -16,6 +18,7 @@ class DashboardScreen extends StatefulWidget {
 
 class _DashboardScreenState extends State<DashboardScreen> {
   int _selectedIndex = 0;
+  bool loading = true;
   bool isFirstTime = true;
   var shell = Shell();
   var nodeVersion;
@@ -26,6 +29,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
     ''').then((result) => {
           setState(() {
             nodeVersion = result.first.outText;
+            loading = false;
           })
         });
   }
@@ -106,34 +110,95 @@ class _DashboardScreenState extends State<DashboardScreen> {
                   ),
                 ),
               ),
-            ],
-          ),
-          VerticalDivider(thickness: 1, width: 1),
-          // This is the main content.
-          Expanded(
-            child: Column(
-              children: [
-                ListTile(
-                  leading: Text(
-                    "Node version",
+              NavigationRailDestination(
+                icon: Icon(Icons.bar_chart_outlined),
+                selectedIcon: Icon(Icons.bar_chart, color: Color(0xff285981)),
+                label: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Reports',
                     style: GoogleFonts.roboto(
                       fontSize: 12,
-                      color: Color(0xff285981),
-                    ),
-                  ),
-                  tileColor: Colors.white,
-                  title: Text(
-                    nodeVersion,
-                    style: GoogleFonts.roboto(
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold,
                       color: Color(0xff285981),
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.settings_outlined),
+                selectedIcon: Icon(Icons.settings, color: Color(0xff285981)),
+                label: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Settings',
+                    style: GoogleFonts.roboto(
+                      fontSize: 12,
+                      color: Color(0xff285981),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
+          VerticalDivider(thickness: 1, width: 1),
+          // This is the main content.
+          loading
+              ? LoaderWidget()
+              : Expanded(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: <Widget>[
+                            Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  // // border: Border.all(
+                                  // //   color: Color(0xff285981),
+                                  // ),
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10),
+                                  ),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey,
+                                      blurRadius: 2.0,
+                                    ),
+                                  ]),
+                              width: MediaQuery.of(context).size.width * .1,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      "Node version",
+                                      style: GoogleFonts.roboto(
+                                        fontSize: 10,
+                                        color: Color(0xff285981),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5,
+                                    ),
+                                    Text(
+                                      nodeVersion,
+                                      style: GoogleFonts.roboto(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                        color: Color(0xff285981),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
         ],
       ),
     );
---
+++ lib/widgets/loader_widget.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+
+class LoaderWidget extends StatelessWidget {
+  const LoaderWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          LoadingAnimationWidget.dotsTriangle(
+              color: Color(0xff285981), size: 50)
+        ],
+      ),
+    );
+  }
+}
